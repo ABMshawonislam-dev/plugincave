fddba337a8a71cc5b019db32131d789c3a694d6e
ABMshawonislam-dev <abmshawonislam@gmail.com>
2020-10-29 15:36:10 +0600

    single typejs done

--- js/script.js
+++ js/script.js
@@ -12,13 +12,16 @@ function isInViewport(element) {
     );
 }
 // find viewport end
-//counterup start
 window.onload  = function () {
     mainArr.map(item=>{
     if (isInViewport(item)) {
         
         if (item.dataset.view) {
             let pluginName = item.dataset.name
+            let progressBarPluginHeight = item.dataset.height
+            let progressBarPluginPercent = item.dataset.parcent
+            let progressBarPluginBg = item.dataset.bg
+            let pluginPercentMove = item.dataset.pmove
             let pluginRunSpeed = item.dataset.speed
             if(pluginName === "counterup"){
                 let counterNumber = item.innerHTML
@@ -34,15 +37,33 @@ window.onload  = function () {
                 let stop = setInterval(()=>{
                     countRunner()
                 },pluginRunSpeed)
+            }else if(pluginName === "progressbar"){
+                let barStartPoint = 0
+                function progressbar(){
+                    barStartPoint = barStartPoint + .1
+                    if(pluginPercentMove == "true"){
+                        item.style.position = 'relative'
+                         item.innerHTML = `<div class="percentages" style="position:absolute;right:0;top:0;">${parseInt(barStartPoint)}</div>`
+                    }else {
+                         item.innerHTML = `<div class="percentages">${parseInt(barStartPoint)}</div>`
+                    }
+                    
+                    item.style.width = `${barStartPoint}%`
+                    item.style.height = `${progressBarPluginHeight}`
+                item.style.background = `${progressBarPluginBg}`
+                if(barStartPoint>progressBarPluginPercent){
+                    clearInterval(stop)
+                }
+                }
+                let stop = setInterval(()=>{
+                    progressbar()
+                },pluginRunSpeed)
             }
-            
             item.dataset.view = ""
         }
     }
 })
 }
-
-
     
 window.onscroll  = function () {
     mainArr.map(item=>{
@@ -50,6 +71,10 @@ window.onscroll  = function () {
         
         if (item.dataset.view) {
             let pluginName = item.dataset.name
+            let progressBarPluginHeight = item.dataset.height
+            let progressBarPluginPercent = item.dataset.parcent
+            let progressBarPluginBg = item.dataset.bg
+            let pluginPercentMove = item.dataset.pmove
             let pluginRunSpeed = item.dataset.speed
             if(pluginName === "counterup"){
                 let counterNumber = item.innerHTML
@@ -65,6 +90,58 @@ window.onscroll  = function () {
                 let stop = setInterval(()=>{
                     countRunner()
                 },pluginRunSpeed)
+            }else if(pluginName === "progressbar"){
+                let barStartPoint = 0
+                function progressbar(){
+                    barStartPoint = barStartPoint + .1
+                    if(pluginPercentMove == "true"){
+                        item.style.position = 'relative'
+                         item.innerHTML = `<div class="percentages" style="position:absolute;right:0;top:0;">${parseInt(barStartPoint)}</div>`
+                    }else {
+                         item.innerHTML = `<div class="percentages">${parseInt(barStartPoint)}</div>`
+                    }
+                    
+                    item.style.width = `${barStartPoint}%`
+                    item.style.height = `${progressBarPluginHeight}`
+                item.style.background = `${progressBarPluginBg}`
+                if(barStartPoint>progressBarPluginPercent){
+                    clearInterval(stop)
+                }
+                }
+                let stop = setInterval(()=>{
+                    progressbar()
+                },pluginRunSpeed)
+            }else if(pluginName === "typejs"){
+                let typeText = item.innerHTML
+                let typeArr = item.innerHTML.split("")
+                let countstartPoint = -1
+                item.innerHTML =""
+                function typejs(){
+                    if(countstartPoint<typeText.length){
+                        countstartPoint++
+                            item.innerHTML += typeText.charAt(countstartPoint)
+                            typeArr = item.innerHTML.split("")
+                    }else{
+                        typeArr.pop()
+                        item.innerHTML = typeArr.join("")
+                         if(typeArr.length==0){
+                            countstartPoint=-1
+                         }
+                    }
+                    
+                    
+                }
+                
+
+                let stop = setInterval(() => {
+                    typejs()
+                }, 200);
+
+
+              
+                
+                
+                
             }
             item.dataset.view = ""
         }
@@ -72,78 +149,3 @@ window.onscroll  = function () {
 })
 }
 //counterup start
-
-//progressbar start
-window.onload  = function () {
-    mainArr.map(item=>{
-    if (isInViewport(item)) {
-        if (item.dataset.view) {
-            let pluginName = item.dataset.name
-            let progressBarPluginHeight = item.dataset.height
-            let progressBarPluginPercent = item.dataset.parcent
-            let progressBarPluginBg = item.dataset.bg
-            let pluginRunSpeed = item.dataset.speed
-            
-            if(pluginName === "progressbar"){
-                let barStartPoint = 0
-                function progressbar(){
-                    barStartPoint = barStartPoint + .1
-                    item.innerHTML = `<div>${parseInt(barStartPoint)}</div>`
-                    item.style.width = `${barStartPoint}%`
-                    item.style.height = `${progressBarPluginHeight}`
-                item.style.background = `${progressBarPluginBg}`
-                
-                if(barStartPoint>progressBarPluginPercent){
-                    clearInterval(stop)
-                }
-                }
-                
-        
-                // item.style.width = `100px`
-                
-                let stop = setInterval(()=>{
-                    progressbar()
-                },pluginRunSpeed)
-            }
-            item.dataset.view = ""
-        }
-    }
-})
-}
-
-window.onscroll  = function () {
-    mainArr.map(item=>{
-    if (isInViewport(item)) {
-        if (item.dataset.view) {
-            let pluginName = item.dataset.name
-            let progressBarPluginHeight = item.dataset.height
-            let progressBarPluginPercent = item.dataset.parcent
-            let progressBarPluginBg = item.dataset.bg
-            let pluginRunSpeed = item.dataset.speed
-            
-            if(pluginName === "progressbar"){
-                let barStartPoint = 0
-                function progressbar(){
-                    barStartPoint = barStartPoint + .1
-                    item.innerHTML = `<div class="percentages">${parseInt(barStartPoint)}</div>`
-                    item.style.width = `${barStartPoint}%`
-                    item.style.height = `${progressBarPluginHeight}`
-                item.style.background = `${progressBarPluginBg}`
-                if(barStartPoint>progressBarPluginPercent){
-                    clearInterval(stop)
-                }
-                }
-                
-        
-                // item.style.width = `100px`
-                
-                let stop = setInterval(()=>{
-                    progressbar()
-                },pluginRunSpeed)
-            }
-            item.dataset.view = ""
-        }
-    }
-})
-}
-//progressbar end
