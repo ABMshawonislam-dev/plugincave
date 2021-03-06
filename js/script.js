c50410013403c804303ca562d4bcd1641f691c81
ABMshawonislam-dev <abmshawonislam@gmail.com>
2021-03-06 16:17:49 +0600

    unminified

--- js/script.js
+++ js/script.js
@@ -1,1 +1,340 @@
-let main=document.querySelectorAll(".abmshawon"),mainArr=Array.from(main);function isInViewport(e){const t=e.getBoundingClientRect();return t.top>=0&&t.left>=0&&t.bottom<=(window.innerHeight||document.documentElement.clientHeight)&&t.right<=(window.innerWidth||document.documentElement.clientWidth)}let menu=document.querySelector(".menu");window.addEventListener("scroll",function(){let e=window.pageYOffset,t=window.pageYOffset+menu.getBoundingClientRect().top;console.log(t),e>t?menu.classList.add("menufixed"):menu.classList.remove("menufixed")}),mainArr.map(e=>{let t=e.dataset.name,n=e.dataset.time,a=new Date(n).getTime(),r=setInterval(function(){let n=(new Date).getTime(),i=a-n,s=Math.floor(i/864e5),l=Math.floor(i%864e5/36e5),o=Math.floor(i%36e5/6e4),d=Math.floor(i%6e4/1e3);"counterdownday"===t?e.innerHTML=s:"counterdownhour"===t?e.innerHTML=l:"counterdownmin"===t?e.innerHTML=o:"counterdownsec"===t&&(e.innerHTML=d),i<0&&(clearInterval(r),e.innerHTML="EXPIRED")},1e3);if("snow"===t){document.querySelector("body").style.overflowX="hidden";let t=e.dataset.bg,n=e.dataset.snowamount,a=e.dataset.snowsize,r=e.dataset.snowspeed,s=e.dataset.height,l=e.dataset.width;e.style.background=t;let o=e.getContext("2d"),d=window.innerWidth,c=window.innerHeight;d="auto"===l?window.innerWidth:l,c="auto"===s?window.innerHeight:s,e.width=d,e.height=c;let p=n,u=[];for(i=0;i<p;i++)u.push({x:Math.random()*d,y:Math.random()*c,r:Math.random()*a+2,d:Math.random()+parseInt(r)});setInterval(function(){for(o.clearRect(0,0,d,c),o.fillStyle="white",o.beginPath(),i=0;i<p;i++){let e=u[i];o.moveTo(e.x,e.y),o.arc(e.x,e.y,e.r,0,2*Math.PI,!0)}o.fill(),function(){for(i=0;i<p;i++){let e=u[i];e.y+=Math.pow(e.d,2),e.y>c&&(u[i]={x:e.x,y:0,r:e.r,d:e.d})}}()},25)}else if("bgspreader"===t){let t=e.dataset.bg,n=e.dataset.height;e.style.background=t,e.style.backgroundAttachment="fixed",e.style.height=n,e.style.clipPath="circle(0px at center)",window.addEventListener("scroll",function(){let t=window.scrollY;e.style.clipPath=`circle(${t}px at center)`})}else if("popup"===t){let t=e.dataset.popid,n=e.dataset.bg,i=document.querySelector(`${t}`);i.style.opacity="0",i.style.width="100%",i.style.height="100vh",i.style.background=`${n}`,i.style.position="fixed",i.style.top="0",i.style.left="0",i.style.transform="scale(0)",i.style.transition=".4s",e.addEventListener("click",function(){i.style.transform="scale(1)",i.style.opacity="1"})}else if("popclose"===t){let t=e.dataset.popid,n=document.querySelector(`${t}`);n.style.cursor="pointer",e.addEventListener("click",function(){n.style.transform="scale(0)",n.style.opacity="0"})}else if("topbottom"===t){let t=e.dataset.scrollvalue;console.log(t),e.addEventListener("click",function(){window.scrollTo({top:t})})}}),window.onload=function(){mainArr.map(e=>{if(isInViewport(e)&&e.dataset.view){let t=e.dataset.name,n=e.dataset.height,i=e.dataset.parcent,a=e.dataset.bg,r=e.dataset.pmove,s=e.dataset.speed;if("counterup"===t){let t=e.innerHTML,n=0;let i=setInterval(()=>{e.innerHTML=n++,n>Number(t)&&clearInterval(i)},s)}else if("progressbar"===t){let t=0;let l=setInterval(()=>{t+=.1,"true"===r?(e.style.position="relative",e.innerHTML=`<div class="percentages" style="position:absolute;right:0;top:0;">${parseInt(t)}%</div>`):e.innerHTML=`<div class="percentages">${parseInt(t)}%</div>`,e.style.width=`${t}%`,e.style.height=`${n}`,e.style.background=`${a}`,t>i&&clearInterval(l)},s)}else if("typejs"===t){let t=e.innerHTML,n=e.innerHTML.split(""),i=-1;e.innerHTML="";setInterval(()=>{i<t.length?(i++,e.innerHTML+=t.charAt(i),n=e.innerHTML.split("")):(n.pop(),e.innerHTML=n.join(""),0==n.length&&(i=-1))},200)}e.dataset.view=""}})},window.onscroll=function(){mainArr.map(e=>{if(isInViewport(e)&&e.dataset.view){let t=e.dataset.name,n=e.dataset.height,i=e.dataset.parcent,a=e.dataset.bg,r=e.dataset.pmove,s=e.dataset.speed;if("counterup"===t){let t=e.innerHTML,n=0;let i=setInterval(()=>{e.innerHTML=n++,n>Number(t)&&clearInterval(i)},s)}else if("progressbar"===t){let t=0;let l=setInterval(()=>{t+=.1,"true"==r?(e.style.position="relative",e.innerHTML=`<div class="percentages" style="position:absolute;right:0;top:0;">${parseInt(t)}</div>`):e.innerHTML=`<div class="percentages">${parseInt(t)}</div>`,e.style.width=`${t}%`,e.style.height=`${n}`,e.style.background=`${a}`,t>i&&clearInterval(l)},s)}else if("typejs"===t){let t=e.innerHTML,n=e.innerHTML.split(""),i=-1;e.innerHTML="";setInterval(()=>{i<t.length?(i++,e.innerHTML+=t.charAt(i),n=e.innerHTML.split("")):(n.pop(),e.innerHTML=n.join(""),0==n.length&&(i=-1))},200)}e.dataset.view=""}})};
+let main = document.querySelectorAll(".abmshawon")
+let mainArr = Array.from(main)
+
+// find viewport start
+function isInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+let menu = document.querySelector(".menu")
+window.addEventListener("scroll",function(){
+ 
+    let scrollAmout = window.pageYOffset
+    let elDistanceToTop = window.pageYOffset + menu.getBoundingClientRect().top
+    console.log(elDistanceToTop)
+    if(scrollAmout > elDistanceToTop){
+        menu.classList.add("menufixed")
+    }else{
+        menu.classList.remove("menufixed")
+    }
+    
+})
+
+// find viewport end
+
+mainArr.map(item => {
+   
+    
+    
+    let pluginName = item.dataset.name
+    let times = item.dataset.time
+    let countDownDate = new Date(times).getTime();
+    let x = setInterval(function () {
+        let now = new Date().getTime();
+        let distance = countDownDate - now;
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        if (pluginName === "counterdownday") {
+            item.innerHTML = days
+        } else if (pluginName === "counterdownhour") {
+            item.innerHTML = hours
+        } else if (pluginName === "counterdownmin") {
+            item.innerHTML = minutes
+        } else if (pluginName === "counterdownsec") {
+            item.innerHTML = seconds
+        }
+        if (distance < 0) {
+            clearInterval(x);
+            item.innerHTML = "EXPIRED";
+        }
+    }, 1000);
+
+    if (pluginName === "snow") {
+        let body = document.querySelector("body")
+        body.style.overflowX = "hidden"
+       let bg = item.dataset.bg
+       let snowamount = item.dataset.snowamount
+       let snowsize = item.dataset.snowsize
+       let snowspeed = item.dataset.snowspeed
+       let height = item.dataset.height
+       let width = item.dataset.width
+        item.style.background = bg
+        let ctx = item.getContext("2d");
+
+        
+        let w = window.innerWidth;
+        let h = window.innerHeight;
+        if(width === "auto"){
+            w = window.innerWidth;
+       }else{
+           w = width;
+       }
+        if(height === "auto"){
+             h = window.innerHeight;
+        }else{
+            h = height;
+        }
+        
+
+        item.width = w;
+        item.height = h;
+        // amount of snow
+        let mf = snowamount;
+
+        let flakes = [];
+
+        for (i = 0; i < mf; i++) {
+            flakes.push({
+                x: Math.random() * w,
+                y: Math.random() * h,
+                r: Math.random() * snowsize + 2, // size of snow
+                d: Math.random() + parseInt(snowspeed), //speed of snow
+
+            })
+        }
+
+        function drawFlakes() {
+            ctx.clearRect(0, 0, w, h);
+            ctx.fillStyle = "white";
+            ctx.beginPath();
+            for (i = 0; i < mf; i++) {
+                let f = flakes[i];
+                ctx.moveTo(f.x, f.y);
+                ctx.arc(f.x, f.y, f.r, 0, Math.PI * 2, true);
+            }
+            ctx.fill()
+            moveFlakes()
+        }
+
+        function moveFlakes() {
+
+            for (i = 0; i < mf; i++) {
+                let f = flakes[i];
+                f.y += Math.pow(f.d, 2);
+                if (f.y > h) {
+                    flakes[i] = {
+                        x: f.x,
+                        y: 0,
+                        r: f.r,
+                        d: f.d
+                    };
+                }
+
+            }
+
+
+        }
+        setInterval(drawFlakes, 25);
+    }else if(pluginName === "bgspreader"){
+        let bg = item.dataset.bg
+        let height = item.dataset.height
+        item.style.background = bg;
+        item.style.backgroundAttachment = "fixed";
+        item.style.height = height;
+        item.style.clipPath = "circle(0px at center)";
+        window.addEventListener("scroll",function(){
+            let value = window.scrollY
+            item.style.clipPath = `circle(${value}px at center)`;
+        })
+    }else if(pluginName === "popup"){
+        let id = item.dataset.popid
+        let bg = item.dataset.bg
+        let selector = document.querySelector(`${id}`)
+        selector.style.opacity = "0"
+        selector.style.width = "100%"
+        selector.style.height = "100vh"
+        selector.style.background = `${bg}`
+        selector.style.position = "fixed"
+        selector.style.top = "0"
+        selector.style.left = "0"
+        selector.style.transform = "scale(0)"
+        selector.style.transition = ".4s"
+
+        item.addEventListener("click",function(){
+            selector.style.transform = "scale(1)"
+            selector.style.opacity = "1"
+        })
+    }else if(pluginName === "popclose"){
+        let id = item.dataset.popid
+        let selector = document.querySelector(`${id}`)
+        selector.style.cursor = "pointer"
+        item.addEventListener("click",function(){
+            selector.style.transform = "scale(0)"
+            selector.style.opacity = "0"
+        })
+    }else if(pluginName === "topbottom"){
+        let scrollValue = item.dataset.scrollvalue
+        console.log(scrollValue)
+        item.addEventListener("click",function(){
+            window.scrollTo({top:scrollValue})
+        })
+    }
+
+
+})
+
+
+
+
+
+
+
+
+// viewport part
+window.onload = function () {
+    mainArr.map(item => {
+        if (isInViewport(item)) {
+            if (item.dataset.view) {
+                let pluginName = item.dataset.name
+                let progressBarPluginHeight = item.dataset.height
+                let progressBarPluginPercent = item.dataset.parcent
+                let progressBarPluginBg = item.dataset.bg
+                let pluginPercentMove = item.dataset.pmove
+                let pluginRunSpeed = item.dataset.speed
+                if (pluginName === "counterup") {
+                    let counterNumber = item.innerHTML
+                    let countstartPoint = 0
+
+                    function countRunner() {
+
+                        item.innerHTML = countstartPoint++
+                        if (countstartPoint > Number(counterNumber)) {
+                            clearInterval(stop)
+                        }
+                    }
+                    let stop = setInterval(() => {
+                        countRunner()
+                    }, pluginRunSpeed)
+                } else if (pluginName === "progressbar") {
+                    let barStartPoint = 0
+
+                    function progressbar() {
+                        barStartPoint = barStartPoint + .1
+                        if (pluginPercentMove === "true") {
+                            item.style.position = 'relative'
+                            item.innerHTML = `<div class="percentages" style="position:absolute;right:0;top:0;">${parseInt(barStartPoint)}%</div>`
+                        } else {
+                            item.innerHTML = `<div class="percentages">${parseInt(barStartPoint)}%</div>`
+                        }
+                        item.style.width = `${barStartPoint}%`
+                        item.style.height = `${progressBarPluginHeight}`
+                        item.style.background = `${progressBarPluginBg}`
+                        if (barStartPoint > progressBarPluginPercent) {
+                            clearInterval(stop)
+                        }
+                    }
+                    let stop = setInterval(() => {
+                        progressbar()
+                    }, pluginRunSpeed)
+                } else if (pluginName === "typejs") {
+                    let typeText = item.innerHTML
+                    let typeArr = item.innerHTML.split("")
+                    let countstartPoint = -1
+                    item.innerHTML = ""
+
+                    function typejs() {
+                        if (countstartPoint < typeText.length) {
+                            countstartPoint++
+                            item.innerHTML += typeText.charAt(countstartPoint)
+                            typeArr = item.innerHTML.split("")
+                        } else {
+                            typeArr.pop()
+                            item.innerHTML = typeArr.join("")
+                            if (typeArr.length == 0) {
+                                countstartPoint = -1
+                            }
+                        }
+                    }
+                    let stop = setInterval(() => {
+                        typejs()
+                    }, 200);
+                }
+                item.dataset.view = ""
+            }
+        }
+    })
+}
+
+window.onscroll = function () {
+    mainArr.map(item => {
+        if (isInViewport(item)) {
+
+            if (item.dataset.view) {
+                let pluginName = item.dataset.name
+                let progressBarPluginHeight = item.dataset.height
+                let progressBarPluginPercent = item.dataset.parcent
+                let progressBarPluginBg = item.dataset.bg
+                let pluginPercentMove = item.dataset.pmove
+                let pluginRunSpeed = item.dataset.speed
+                if (pluginName === "counterup") {
+                    let counterNumber = item.innerHTML
+                    let countstartPoint = 0
+
+                    function countRunner() {
+
+                        item.innerHTML = countstartPoint++
+                        if (countstartPoint > Number(counterNumber)) {
+                            clearInterval(stop)
+                        }
+                    }
+                    let stop = setInterval(() => {
+                        countRunner()
+                    }, pluginRunSpeed)
+                } else if (pluginName === "progressbar") {
+                    let barStartPoint = 0
+
+                    function progressbar() {
+                        barStartPoint = barStartPoint + .1
+                        if (pluginPercentMove == "true") {
+                            item.style.position = 'relative'
+                            item.innerHTML = `<div class="percentages" style="position:absolute;right:0;top:0;">${parseInt(barStartPoint)}</div>`
+                        } else {
+                            item.innerHTML = `<div class="percentages">${parseInt(barStartPoint)}</div>`
+                        }
+
+                        item.style.width = `${barStartPoint}%`
+                        item.style.height = `${progressBarPluginHeight}`
+                        item.style.background = `${progressBarPluginBg}`
+                        if (barStartPoint > progressBarPluginPercent) {
+                            clearInterval(stop)
+                        }
+                    }
+                    let stop = setInterval(() => {
+                        progressbar()
+                    }, pluginRunSpeed)
+                } else if (pluginName === "typejs") {
+                    let typeText = item.innerHTML
+                    let typeArr = item.innerHTML.split("")
+                    let countstartPoint = -1
+                    item.innerHTML = ""
+
+                    function typejs() {
+                        if (countstartPoint < typeText.length) {
+                            countstartPoint++
+                            item.innerHTML += typeText.charAt(countstartPoint)
+                            typeArr = item.innerHTML.split("")
+                        } else {
+                            typeArr.pop()
+                            item.innerHTML = typeArr.join("")
+                            if (typeArr.length == 0) {
+                                countstartPoint = -1
+                            }
+                        }
+                    }
+                    let stop = setInterval(() => {
+                        typejs()
+                    }, 200);
+                }
+                item.dataset.view = ""
+            }
+        }
+    })
+}
+//counterup start
